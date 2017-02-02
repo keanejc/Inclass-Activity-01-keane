0a8c92b7e3a510dfa81fca8de06045557d6fa151
keanejc <keanejc@g.cofc.edu>
2017-02-02 17:52:00 -0500

    date picker finished

--- app/src/main/java/com/introtoandriod/inclass_activity_01_keane/MainActivity.java
+++ app/src/main/java/com/introtoandriod/inclass_activity_01_keane/MainActivity.java
@@ -2,12 +2,15 @@ package com.introtoandriod.inclass_activity_01_keane;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
+    final DatePicker date = (DatePicker) findViewById(R.id.DatePicker01);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
