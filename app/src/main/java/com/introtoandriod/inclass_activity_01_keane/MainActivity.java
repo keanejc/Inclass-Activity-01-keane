f8e00f8eeef007f9f8855e4b33f611691a4140e1
keanejc <keanejc@g.cofc.edu>
2017-02-02 18:06:44 -0500

    progress bar and rating bar finished

--- app/src/main/java/com/introtoandriod/inclass_activity_01_keane/MainActivity.java
+++ app/src/main/java/com/introtoandriod/inclass_activity_01_keane/MainActivity.java
@@ -3,14 +3,17 @@ package com.introtoandriod.inclass_activity_01_keane;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.DatePicker;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     final DatePicker date = (DatePicker) findViewById(R.id.DatePicker01);
+    ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgress.setProgress(75);
     }
 
 }
